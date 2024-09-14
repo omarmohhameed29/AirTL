78820e1af9aaa74c6e8ff38e3f788a70e7a7526e
omarmohhameed29 <omarmohhameed828@gmail.com>
2024-09-14 19:42:17 +0300

    resotre default_args config

--- dags/ETL.py
+++ dags/ETL.py
@@ -128,9 +128,11 @@ with DAG(
         python_callable=send_success_mail
     )
 
-    failure_email_task = PythonOperator(
-        task_id='send_failure_email',
-        python_callable=send_failure_mail,
+    failure_email_task = EmailOperator(
+        task_id="failure_email",
+        to=["om0558064@gmail.com"],
+        subject="Task Failed in Airflow DAG",
+        html_content="<h3>One or more tasks in your Airflow DAG have failed.</h3>",
         trigger_rule=TriggerRule.ONE_FAILED
     )
     # Task sequence
