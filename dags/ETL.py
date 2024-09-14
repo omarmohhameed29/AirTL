21ca68ef048141cc84dc2733d563f589ba0c5b8e
omarmohhameed29 <omarmohhameed828@gmail.com>
2024-09-14 19:59:59 +0300

    add task group for load subtasks

--- dags/ETL.py
+++ dags/ETL.py
@@ -10,20 +10,20 @@ from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from datetime import timedelta, datetime
 from airflow.utils.trigger_rule import TriggerRule
+from airflow.utils.task_group import TaskGroup
 
 # Add the path to the plugins directory
 sys.path.append(os.path.join(os.path.dirname(__file__), '../plugins'))
 from fotmob_scrapper import extract, transform
 from mail_util import send_success_mail, send_failure_mail
+
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-
 def pre_execute_callback(context):
     logging.info("Preparing to send email")
     logging.info(f"SMTP connection details: {context['task_instance'].task.conn_id}")
-
 
 def insert_into_table_task_func(**kwargs):
     """
@@ -59,8 +59,8 @@ default_args = {
     'email': ['omarmohhameed828@gmail.com'],
     'email_on_failure': True,
     'email_on_retry': True,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=1),
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
     'retry_exponential_backoff': False,
     'max_retry_delay': timedelta(hours=1),
     'start_date': datetime(2024, 9, 14),
@@ -80,8 +80,7 @@ with DAG(
     # Extract task
     extract_task = PythonOperator(
         task_id="extract",
-        python_callable=extract,
-        provide_context=True
+        python_callable=extract
     )
 
     # Transform task function
@@ -98,41 +97,38 @@ with DAG(
     # Transform task
     transform_task = PythonOperator(
         task_id="transform",
-        python_callable=transform_task_func,
-        provide_context=True
+        python_callable=transform_task_func
     )
 
-    # Create table in Postgres
-    create_table_task = PostgresOperator(
-        task_id="create_table",
-        postgres_conn_id='postgres_conn',  # Connection ID configured in Airflow
-        sql="create_table.sql"
-    )
+    with TaskGroup(group_id='load') as load:
+        # Create table in Postgres
+        create_table_task = PostgresOperator(
+            task_id="create_table",
+            postgres_conn_id='postgres_conn',  # Connection ID configured in Airflow
+            sql="create_table.sql"
+        )
 
-    # Load transformed data into the Postgres table using PostgresHook
-    insert_into_table_task = PythonOperator(
-        task_id='insert_to_table',
-        python_callable=insert_into_table_task_func,
-        provide_context=True
-    )
+        # Load transformed data into the Postgres table using PostgresHook
+        insert_into_table_task = PythonOperator(
+            task_id='insert_to_table',
+            python_callable=insert_into_table_task_func
+        )
 
-    # AllTaskSuccess = EmailOperator (
-    #     trigger_rule=TriggerRule.ALL_SUCCESS,
-    #     task_id="AllTaskSuccess",
-    #     to=["omarmohhameed828@gmail.com"],
-    #     subject="All Task completed successfully",
-    #     html_content='<h3>All Task completed successfully" </h3>')
+        create_table_task >> insert_into_table_task
 
     email_task = PythonOperator(
         task_id='send_success_email',
         python_callable=send_success_mail
     )
 
-    failure_email_task = PythonOperator(
-        task_id='send_failure_email',
-        python_callable=send_failure_mail,
+    failure_email_task = EmailOperator(
+        task_id="failure_email",
+        to=["om0558064@gmail.com"],
+        subject="Task Failed in Airflow DAG",
+        html_content="<h3>One or more tasks in your Airflow DAG have failed.</h3>",
         trigger_rule=TriggerRule.ONE_FAILED
     )
+
     # Task sequence
-    extract_task >> transform_task >> create_table_task >> insert_into_table_task >> email_task
-    [extract_task, transform_task, create_table_task, insert_into_table_task, email_task] >> failure_email_task
+    extract_task >> transform_task >> load >> email_task
+    [extract_task, transform_task, load] >> failure_email_task
